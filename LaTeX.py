0e96b7b27eed57b53323bc7da2f8b214ce44431a
Benedikt Tissot <benedikt@tissot.de>
2020-10-30 11:14:37 +0100

    great enhancements to ensure compiling latex code

---
+++ LaTeX.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+import re  # used to better ensure compiling latex!
+from bs4 import BeautifulSoup  # used to get rid of HTML stuff
+
+# Here we can configure
+# environmants for math
+math_envs = ["equation", "align"]
+math_matchers = ["(\\$)([^\\$]+)(\\$)", "(\\$\\$)([^\\$]+)(\\$\\$)",
+                 "(\\\\\\[)((?:.(?!\\\\\\]))*.)(\\\\\\])", "(\\\\\\()((?:.(?!\\\\\\)))*.)(\\\\\\))",
+                 "(\\\\begin\\{(?P<env>"+"|".join(math_envs)+")\\*?\\})(.+(?!(?P=env)))(\\\\end\\{(?P=env)\\*?\\})"]
+math_regex = re.compile("("+"|".join(math_matchers)+")", flags=re.DOTALL)
+
+# supports regexp (needs to be escaped accordingly)
+# the general sub bevore is applied bevore \\ are stripped
+general_sub_bevore = [('\\\\"o', "ö"),
+                      ('\\\\"a', "ä"),
+                      ('\\\\"u', "ü")]
+# the general sub is applied last after \\ are stripped
+general_sub = [("cite\\{([^}]+)\\}", "[\\1]"),  # citekeys are pointless as we do not have the bib file
+               ("\\\\*mathbit", ""),  # we need the backslash, else we can be left with a backslash that destroys everything
+               ("o", "o")]
+# Add things that should have a backslash here, as we strip all backslashes outside math
+# right now this does not support keeping the matches of groups
+outside_math_sub = [("{\\\\deg}", "$^{\\\\circ}$"),
+                          ("\\\\'", "\\\\'")]
+# latex commands that do not make sense without the backslash (no regexp)
+prepend_backslash = ["emph\\{", "textit\\{", "textbf\\{", "^", "_", "&" ,"$", "%"]
+# ensure no unescaped %
+for command in prepend_backslash:
+    outside_math_sub.append(((re.compile("\\\\*"+re.escape(command))), "\\\\"+command))
+# here we ensure no newlines and tabbing in math
+inside_math_sub = [("\\\\\\\\", "\\\\ "),  # newline to space
+                   ("([^\\\\])%", "\\1\\\\%"),  # escape %
+                   ("([^\\\\])#", "\\1\\\\%"),  # escape #
+                   ("\\&", ""),  # no amperes and
+                   ("\\\\left", ""),  # no left and rights, just to be on the save side
+                   ("\\\\right", "")]
+
+# we strip backslashes from commands not in the whitelist to ensure we do not get problems with custom commands
+# Mind that we only strip backslashes infront of a-z, A-Z, and 0-9
+math_command_whitelist = [
+    "alpha", "aleph", "beta", "chi", "delta", "partial", "epsilon", "varepsilon", "exists", "eta", "gamma", "kappa", "lambda", "mu", "nu", "nabla", "omega", "pi", "varpi", "psi", "varphi", "phi", "tau", "theta", "vartheta", "rho", "varrho", "sigma", "upsilon", "varsigma", "vee", "xi", "zeta"
+    "ll", "leq", "le", "gg", "geq", "ge", "approx", "equiv", "simeq", "sim",
+    "cdot", "cdots", "dots",
+    "dag", "infty", "hbar"
+    "Delta", "Gamma", "Omega", "Lambda", "Phi", "Pi", "Psi", "Sigma", "Theta", "Upsilon", "Xi", "Zeta",
+    "Re", "Im", "Vert",
+    "Leftrightarrow", "Leftarrow", "Rightarrow", "Longleftrightarrow", "Longleftarrow", "Longrightarrow", "wedge",
+    "to", "leftrightarrow", "leftarrow", "rightarrow", "longleftrightarrow", "longleftarrow", "longrightarrow", "uparrow", "downarrow",
+    "parallel", "perp", "mapsto", "longmapsto", "not", "prime",
+    "binom", "sqrt", "overset", "int", "frac", "in",
+    "tan", "cos", "exp", "ln", "sin",
+    "sum", "cup", "times", "prod", "otimes", "propto", "circ", "setminus", "forall", "emptyset", "wedge", "subset", "supset",
+    "quad", "qquad", "hat", "langle", "rangle",
+    # "bra", "ket", "norm", "abs", "braket", "comm", "acomm", "proj", "ev", "eval",  # these are actually not avaible
+    "mathrm", "text", "mathbf"]
+math_env_whitelist = [ "cases", "matrix", "pmatrix", "array" ]
+
+
+def find_all_math(s, math_regex=math_regex):
+    """Search for all valid latex environments, returns a list of list [[whole match, begin, content, end],...]"""
+    math_matches = math_regex.findall(s)
+    math_matches = [[j for j in i if j != ""] for i in math_matches]
+    for i in math_matches:
+        if len(i) == 5:
+            del i[2]
+    return math_matches
+
+
+def elc(s, general_sub=general_sub,
+        outside_math_sub=outside_math_sub,
+        inside_math_sub=inside_math_sub,
+        math_command_whitelist=math_command_whitelist,
+        math_env_whitelist=math_env_whitelist):
+    """Ensure Latex compatibility of a string s"""
+    # first use BeautifulSoup to get rid of html artefacts
+    ret = BeautifulSoup(s, "html.parser").text.strip()
+
+    ret = re.sub("\\$\\\\require\\{[^\\]\\}]+\\}\\$", "", ret)   # require is used in math by MATHJAX to load additional packages
+
+    if (len(re.findall("\\{", ret)) != len(re.findall("\\}", ret))):
+        return "Amount of curly braces not balanced."
+    if (len(re.findall("\\\\\\{", ret)) != len(re.findall("\\\\\\}", ret))):
+        return "Amount of curly braces not balanced."
+
+    math_matches = find_all_math(s)
+    for i, match in enumerate(math_matches):
+        ret = ret.replace(match[0], "MATH{}MATH".format(i))
+        for find, replace in inside_math_sub:
+            match[2] = re.sub(find, replace, match[2])
+        # try to clever strip "bad" backslashes:
+        for command in math_command_whitelist:
+            # for backslash after a command add a space bevore the backslash to ensure commands are split
+            match[2] = re.sub(command+"(\\\\)", command+" \\1", match[2])
+            # after a command a backslash, whitespace, a bracket or the end of the string are allowed
+            match[2] = re.sub("\\\\"+command+"(\\\\|\\s|\\{|\\}|\\(|\\)|\\[|\\]|\\Z)", "\\\\\\\\"+command+"\\1", match[2])
+        regex = "(\\\\begin\\{(?P<env>"+"|".join(math_env_whitelist)+")\\*?\\})(.+(?!(?P=env)))(\\\\end\\{(?P=env)\\*?\\})"
+        match[2] = re.sub(regex, "\\\\\\1\\3\\\\\\4", match[2])
+        match[2] = re.sub("\\\\([a-zA-Z0-9])", "\\1", match[2])
+
+    for i, finrep in enumerate(outside_math_sub):
+        ret = re.sub(finrep[0], "OUTSIDE{}OUTSIDE".format(i), ret)
+
+    for find, replace in general_sub_bevore:
+        ret = re.sub(find, replace, ret)
+
+    ret = ret.replace("\\", "")
+
+    for i, finrep in enumerate(outside_math_sub):
+        ret = re.sub("OUTSIDE{}OUTSIDE".format(i), finrep[1], ret)
+
+    for i, match in enumerate(math_matches):
+        ret = ret.replace("MATH{}MATH".format(i), "$"+match[2]+"$")
+
+    for find, replace in general_sub:
+        ret = re.sub(find, replace, ret)
+
+    return ret.strip()
--- getcontent.py
+++ getcontent.py
@@ -18,12 +18,11 @@
 import feedparser as fp
 import requests
 import datetime
-import re
 import bs4
 from bs4 import BeautifulSoup
 import sys
 import time  # used for connections to arxiv
-
+from LaTeX import elc
 # arxiv
 # see arxiv API for options, here we search for cond-mat and quant-ph articles
 # (logical +OR+)
@@ -41,118 +40,22 @@ science_weekly = ["science"]
 science_monthly = ["advances"]
 
 
-# Here we can configure
-# environmants for math
-latex_math_envs = ["equation", "align"]
-latex_math_matchers = ["(\\$)([^\\$]+)(\\$)", "(\\$\\$)([^\\$]+)(\\$\\$)",
-                       "(\\\\\\[)((?:.(?!\\\\\\]))*.)(\\\\\\])", "(\\\\\\()((?:.(?!\\\\\\)))*.)(\\\\\\))",
-                       "(\\\\begin\\{(?P<env>"+"|".join(latex_math_envs)+")\\*?\\})(.+(?!(?P=env)))(\\\\end\\{(?P=env)\\*?\\})"]
-latex_math_regex = re.compile("("+"|".join(latex_math_matchers)+")", flags=re.DOTALL)
-
-# supports regexp (needs to be escaped accordingly)
-# the general sub is applied last after \\ are stripped
-latex_general_sub = [("cite\\{([^}]+)\\}", "[\\1]"),  # citekeys are pointless as we do not have the bib file
-                     ("\\\\*mathbit", ""),  # we need the backslash, else we can be left with a backslash that destroys everything
-                     ("o", "o")]
-# Add things that should have a backslash here, as we strip all backslashes outside math
-# right now this does not support keeping the matches of groups
-latex_outside_math_sub = [("{\\\\deg}", "$^{\\\\circ}$"),
-                          ("\\\\'", "\\\\'")]
-# latex commands that do not make sense without the backslash (no regexp)
-latex_prepend_backslash = ["emph\\{", "textit\\{", "textbf\\{", "^", "_", "&" ,"$", "%"]
-# ensure no unescaped %
-for command in latex_prepend_backslash:
-    latex_outside_math_sub.append(((re.compile("\\\\*"+re.escape(command))), "\\\\"+command))
-# here we ensure no newlines and tabbing in math
-# , ("\\\\begin\\{aligned\\}(.+(?!aligned))\\\\end\\{aligned\\}", "\\1")
-latex_inside_math_sub = [("\\\\\\\\", "\\\\ "),  # newline to space
-                         ("([^\\\\])%", "\\1\\\\%"),  # escape %
-                         ("([^\\\\])#", "\\1\\\\%"),  # escape #
-                         ("\\&", ""),
-                         # whitelist of capital letter commands
-                         # Double escape, because in the end we strip some \
-                         ("(\\\\Delta)", "\\\\\\1"),
-                         ("(\\\\Gamma)", "\\\\\\1"),
-                         ("(\\\\Omega)", "\\\\\\1"),
-                         ("(\\\\Lambda)", "\\\\\\1"),
-                         ("(\\\\Phi)", "\\\\\\1"),
-                         ("(\\\\Pi)", "\\\\\\1"),
-                         ("(\\\\Psi)", "\\\\\\1"),
-                         ("(\\\\Sigma)", "\\\\\\1"),
-                         ("(\\\\Theta)", "\\\\\\1"),
-                         ("(\\\\Upsilon)", "\\\\\\1"),
-                         ("(\\\\Vert)", "\\\\\\1"),
-                         ("(\\\\Xi)", "\\\\\\1"),
-                         ("(\\\\Leftrightarrow)", "\\\\\\1"),
-                         ("(\\\\Leftarrow)", "\\\\\\1"),
-                         ("(\\\\Rightarrow)", "\\\\\\1"),
-                         ("(\\\\Longleftrightarrow)", "\\\\\\1"),
-                         ("(\\\\Longleftarrow)", "\\\\\\1"),
-                         ("(\\\\Longrightarrow)", "\\\\\\1"),
-                         ("(\\\\Im)", "\\\\\\1"),
-                         ("(\\\\Re)", "\\\\\\1"),
-                         # if something does not start with a small letter it probably is not a latex command
-                         ("\\\\([A-Z0-9])", "\\1")]
-
-
-def find_all_latex_math(s, math_regex=latex_math_regex):
-    """Search for all valid latex environments, returns a list of list [[whole match, begin, content, end],...]"""
-    math_matches = latex_math_regex.findall(s)
-    math_matches = [[j for j in i if j != ""] for i in math_matches]
-    for i in math_matches:
-        if len(i) == 5:
-            del i[2]
-    return math_matches
-
-
-def elc(s, general_sub=latex_general_sub,
-        outside_math_sub=latex_outside_math_sub,
-        inside_math_sub=latex_inside_math_sub):
-    """Ensure Latex compatibility of a string s"""
-    # first use BeautifulSoup to get rid of html artefacts
-    ret = BeautifulSoup(s, "html.parser").text.strip()
-
-    ret = re.sub("\\$\\\\require\\{[^\\]\\}]+\\}\\$", "", ret)   # require is used in math by MATHJAX to load additional packages
-
-    if (len(re.findall("\\{", ret)) != len(re.findall("\\}", ret))):
-        return "Amount of curly braces not balanced."
-    if (len(re.findall("\\\\\\{", ret)) != len(re.findall("\\\\\\}", ret))):
-        return "Amount of curly braces not balanced."
-
-    math_matches = find_all_latex_math(s)
-    for i, match in enumerate(math_matches):
-        ret = ret.replace(match[0], "MATH{}MATH".format(i))
-        for find, replace in inside_math_sub:
-            match[2] = re.sub(find, replace, match[2])
-
-    for i, finrep in enumerate(outside_math_sub):
-        ret = re.sub(finrep[0], "OUTSIDE{}OUTSIDE".format(i), ret)
-
-    ret = ret.replace("\\", "")
-
-    for i, finrep in enumerate(outside_math_sub):
-        ret = re.sub("OUTSIDE{}OUTSIDE".format(i), finrep[1], ret)
-
-    for i, match in enumerate(math_matches):
-        ret = ret.replace("MATH{}MATH".format(i), "$"+match[2]+"$")
-
-    for find, replace in general_sub:
-        ret = re.sub(find, replace, ret)
-
-    return ret.strip()
-
-
 class Article:
     """
     Container for an article (title, url, date, authors, summary, journal)
     """
-    def __init__(self, title, url, date, authors, summary, journal):
-        self.title = elc(title)
+    def __init__(self, title, url, date, authors, summary, journal, ensure_latex=True):
+        def _elc(s):
+            if ensure_latex:
+                return elc(s)
+            else:
+                return s
+        self.title = _elc(title)
         self.url = url
         self.date = date
-        self.authors = [elc(a).replace("}", "").replace("{", "") for a in authors]
-        self.summary = elc(summary)
-        self.journal = elc(journal)
+        self.authors = [_elc(a).replace("}", "").replace("{", "") for a in authors]
+        self.summary = _elc(summary)
+        self.journal = _elc(journal)
 
 
     def author_string(self, max_authors=3):
@@ -292,8 +195,9 @@ def parsed_datetime(parsed_date):
 
 def get_arxivarticles(enddate=datetime.date.today(),
                       startdate=datetime.date.today() - datetime.timedelta(days=8),
-                      query=arxivquery):
-    """get arxiv articles from startdate 00:00 to enddate 00:00"""
+                      query=arxivquery, id_list=""):
+    """get arxiv articles from startdate 00:00 to enddate 00:00.
+    If an id_list is provided start and end dates are ignored."""
     feeds = []
     cond = True
     retry = 0
@@ -302,8 +206,13 @@ def get_arxivarticles(enddate=datetime.date.today(),
         # We use the latter as we are only interested in new papers
         # see https://github.com/ContentMine/getpapers/issues/180
         # and https://github.com/ContentMine/getpapers/wiki/arxiv-query-format
-        url = "https://export.arxiv.org/api/query?search_query=(" + query + ")+AND+submittedDate:["
-        url += startdate.strftime("%Y%m%d")+"0000+TO+"+enddate.strftime("%Y%m%d")+"0000]"
+        url = "https://export.arxiv.org/api/query?"
+        if query != "":
+            url += "search_query=(" + query + ")+AND+"
+        if id_list != "":
+            url += "id_list=" + id_list
+        else:
+            url += "submittedDate:["+startdate.strftime("%Y%m%d")+"0000+TO+"+enddate.strftime("%Y%m%d")+"0000]"
         url += "&start="+str(100*len(feeds))+"&max_results=100&sortBy=submittedDate&sortOrder=descending"
         feeds.append(fp.parse(url))
         totalresults = int(feeds[-1]["feed"]["opensearch_totalresults"])
@@ -316,7 +225,6 @@ def get_arxivarticles(enddate=datetime.date.today(),
             cond = False
         if cond:  # arxiv asks users to wait for 3 seconds between queries
             time.sleep(3)
-
 
     articles = []
     for feed in feeds:

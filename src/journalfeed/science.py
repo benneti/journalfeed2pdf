88c5cd1aec9f23a98db2dcf87b7a01ccb8cf140c
Benedikt Tissot <benedikt@tissot.fun>
2026-01-09 08:57:38 +0100

    ensure no new lines in author names for science

--- src/journalfeed/science.py
+++ src/journalfeed/science.py
@@ -26,7 +26,7 @@ def get_articles(enddate = datetime.date.today(),
                 authors = []
                 try:
                     for aths in e.authors:
-                        aths = aths["name"].strip().replace(".\u2009T.", ".").replace(" and ", ", ")
+                        aths = aths["name"].strip().replace(".\u2009T.", ".").replace(" and ", ", ").replace("\n", "")
                         for author in aths.split(", "):
                             author = author.strip()
                             if author != "":
